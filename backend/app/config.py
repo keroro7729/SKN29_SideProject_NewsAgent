06999c0faf6bfc24c72ae785887eee3988b8ed96
keroro7729 <keroro06108@gmail.com>
2026-04-15 09:50:25 +0900

    naver_client 싱글톤 추가, 중복로직,  불필요한 주석 제거

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,3 @@ load_dotenv()
 NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID", "")
 NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET", "")
 
-NAVER_NEWS_API_URL = "https://openapi.naver.com/v1/search/news.json"
-
-
